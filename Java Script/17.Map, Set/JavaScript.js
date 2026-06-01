451870cb272b47f17d0ed49d2e52e436cb0b103f
ud-nirob3281 <webdeveloper32811290@gmail.com>
2026-06-01 08:34:32 +0600

    Update JavaScript.js

--- Java Script/17.Map, Set/JavaScript.js
+++ Java Script/17.Map, Set/JavaScript.js
@@ -171,7 +171,7 @@ console.log(wMap);  // WeakMap খালি
 ✅ Size property নেই*/
 
 //WeakSet উদাহরণ:
-JavaScript
+
 const onlineUsers = new WeakSet();
 
 let user1 = { name: 'Alice' };
@@ -185,70 +185,7 @@ console.log(onlineUsers.has(user1));  // true
 user1 = null;  // এখন WeakSet থেকে সরে যায়
 console.log(onlineUsers.has(user1));  
 
-//! Task
-//1
-{
-  const studentMap = new Map([
-    [1, 'Nirob'],
-    [2, 'Safa'],
-    [3, 'Fatema'],
-    [4, 'Raj'],
-    [5, 'Shuvo'],
-  ]);
-  // console.log(studentMap.get(2));
-  studentMap.delete(4);
-  // console.log(studentMap);
-
-  //2
-  const langSet = new Set(['Python', 'PHP', 'Rust', 'JavaScript', 'C', 'PHP']);
-  //console.log(langSet);
-  // langSet.values().forEach(val => console.log(val));
-  //4
-  const conTactMap = new Map([
-    [0134, 'Nirob'],
-    [5414, 'Safa'],
-    [4554, 'Raj'],
-  ]);
-  conTactMap.set(4544, 'Shuvo');
-  conTactMap.set(0134, 'Akash');
-  conTactMap.delete(4554);
-  conTactMap.get(5414);
-  //5
-  const fruits = ['apple', 'banana', 'apple', 'orange', 'banana'];
-  const reDu = new Set(fruits);
-  //console.log(reDu);
-  //6
-  const loginUser = new Set();
-  const newUserId1 = 1;
-  const newUserId2 = 2;
-  const newUserId3 = 3;
-  const newUserId4 = 4;
-  const newUserId5 = 5;
-  //Login user
-  loginUser.add(newUserId1);
-  loginUser.add(newUserId2);
-  loginUser.add(newUserId3);
-  loginUser.add(newUserId4);
-  loginUser.add(newUserId5);
-  //Log out user
-  loginUser.delete(newUserId3);
-  loginUser.delete(newUserId1);
-  //Specifiq Currently Login User
-  //console.log(loginUser.has(newUserId3));
-  //console.log(loginUser.has(newUserId5));
-
-  //7
-  const book = new Map([
-    ['Success', 'Nirob'],
-    ['Be Creative', 'Safa'],
-    ['First Night', 'Safayet'],
-    ['Marrige', 'Shuvo'],
-    ['Kill Your Addiction', 'Nirob'],
-  ]);
-  book.set('First Night', 'Raj');
-  //console.log(book.size);
-  //10
-  const shopingCart = new Map([
+//  const shopingCart = new Map([
     [1, 10],
     [2, 3],
     [3, 8],
